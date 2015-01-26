c9ad71d187eccc00a3323a201dd26e5175c6e8e9
steve henderson <steve@stratosforge.net>
2015-01-26 08:59:26 -0500

    initial code add

---
+++ Assets/DrawWebcamTexture.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawWebcamTexture : MonoBehaviour {
+
+	public void Start () {
+		WebCamTexture webcam = new WebCamTexture();
+		GameObject bgimg = GameObject.Find("BackgroundImage");
+		bgimg.guiTexture.texture = webcam;
+		webcam.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
